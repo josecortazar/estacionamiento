f6fabfcdedc35af12a6cca7d8ef0ee717a229431
Jose Gabriel Cortazar Ocampo - Ceiba Software <jose.cortazar@ceiba.com.co>
2021-07-15 11:40:19 -0500

    Primer Commit

---
+++ src/test/java/com/estacionamiento/EstacionamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estacionamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
