33f75d5739e408f97c3e57833e24bfd5a052d32d
josecortazar <jose.cortazar@ceiba.com.co>
2021-07-16 08:12:22 -0500

    Comentario de plugin UCD Detector

--- src/main/java/com/estacionamiento/EstacionamientoApplication.java
+++ src/main/java/com/estacionamiento/EstacionamientoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EstacionamientoApplication {
+public class EstacionamientoApplication { // NO_UCD (unused code)
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstacionamientoApplication.class, args);
